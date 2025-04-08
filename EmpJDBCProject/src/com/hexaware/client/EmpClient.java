f7426d949864c3f5827a06851463c76932a28039
tharnidhar <tharnidhar.csgrad@gmail.com>
2025-04-08 12:20:03 +0530

    added display method

--- EmpJDBCProject/src/com/hexaware/client/EmpClient.java
+++ EmpJDBCProject/src/com/hexaware/client/EmpClient.java
@@ -4,7 +4,10 @@ public class EmpClient{
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-		
+		display();
+	}
+	public static void display() {
+		System.out.println("Display");
 	}
 
 }
